bc8120d7b16e3834072619f0543d98429aceea15
samuelvergaramartin <samuelvergaramartin@protonmail.com>
2025-10-02 09:30:21 +0200

    Updated main branch

--- Tema 3/Ejercicios/Ejercicio13/app/src/main/java/com/example/ejercicio13/MainActivity.java
+++ Tema 3/Ejercicios/Ejercicio13/app/src/main/java/com/example/ejercicio13/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.ejercicio13;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.CompoundButton;
+import android.widget.ProgressBar;
 import android.widget.RatingBar;
+import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -25,9 +34,62 @@ public class MainActivity extends AppCompatActivity {
         });
 
         final Spinner spinner = findViewById(R.id.spinner);
+        final RatingBar ratingBar = findViewById(R.id.ratingBar);
+        final ProgressBar progressBar1 = findViewById(R.id.progressBar1);
+        final ProgressBar progressBar2 = findViewById(R.id.progressBar2);
+        final SeekBar  seekBar  = findViewById(R.id.seekBar);
+        final Switch switch1 = findViewById(R.id.switch1);
+        final AutoCompleteTextView autoCompleteTextView = findViewById(R.id.autocomplete);
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.ciudades, android.R.layout.simple_spinner_item);
         spinner.setAdapter(adapter);
 
+        final String[] opciones = {"Opcion 1", "Opcion 2", "Opcion 3", "Opcion 4"};
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, opciones);
 
+        autoCompleteTextView.setAdapter(adapter2);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Ciudad seleccionada: " + parent.getSelectedItem().toString(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Toast.makeText(MainActivity.this, "La puntuación  seleccionada es: " + rating, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Toast.makeText(MainActivity.this, "Progreso: " + progress, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(MainActivity.this, "Iniciando cambio de proceso", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(MainActivity.this, "Cambio de proceso finalizado", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(MainActivity.this, "Estado del switch: " + isChecked, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
