ac46f1514c90acbebbdfc669c516bcc32f687eaf
samuelvergaramartin <samuelvergaramartin@protonmail.com>
2025-11-04 21:33:19 +0100

    Updated main branch

---
+++ Tema 5/Ejercicios/Ejercicio1/app/src/main/java/com/example/ejercicio1/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.ejercicio1;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        final ListView listado = findViewById(R.id.listado);
+        final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.listado_options, android.R.layout.simple_list_item_1);
+
+        listado.setAdapter(adapter);
+
+
+    }
+}
---
+++ Tema 5/Ejercicios/Ejercicio3/app/src/main/java/com/example/ejercicio3/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.ejercicio3;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        final TextView texto = findViewById(R.id.texto);
+        final GridView listado = findViewById(R.id.listado);
+        final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.series, android.R.layout.simple_list_item_1);
+        final String textoPorDefecto = texto.getText().toString();
+
+        listado.setAdapter(adapter);
+
+        listado.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                texto.setText(textoPorDefecto + " " + parent.getItemAtPosition(position).toString());
+            }
+        });
+    }
+}
