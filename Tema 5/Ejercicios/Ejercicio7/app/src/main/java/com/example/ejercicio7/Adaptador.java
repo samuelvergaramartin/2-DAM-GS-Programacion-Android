14926f10de663700027d0f530daea6d1c1b0ca54
samuelvergaramartin <samuelvergaramartin@protonmail.com>
2025-11-15 20:43:56 +0100

    Updated main branch

--- Tema 5/Ejercicios/Ejercicio7/app/src/main/java/com/example/ejercicio7/Adaptador.java
+++ Tema 5/Ejercicios/Ejercicio7/app/src/main/java/com/example/ejercicio7/Adaptador.java
@@ -1,10 +1,13 @@
 package com.example.ejercicio7;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
@@ -37,6 +40,15 @@ public class Adaptador extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        LayoutInflater layoutInflater = LayoutInflater.from(contexto);
+        View elemento = layoutInflater.inflate(R.layout.elemento, parent, false);
+
+        final ImageView imagen = elemento.findViewById(R.id.imagen);
+        imagen.setImageResource(datos.get(position).getImagen());
+
+        final TextView texto = elemento.findViewById(R.id.texto);
+        texto.setText(datos.get(position).getTexto());
+
+        return elemento;
     }
 }
--- Tema 5/Ejercicios/Ejercicio7/app/src/main/java/com/example/ejercicio7/MainActivity.java
+++ Tema 5/Ejercicios/Ejercicio7/app/src/main/java/com/example/ejercicio7/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.ejercicio7;
 
 import android.os.Bundle;
+import android.widget.GridLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +25,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ArrayList<Datos> datos = new ArrayList<>();
+        datos.add(new Datos(R.drawable.buho, "Buho"));
+        datos.add(new Datos(R.drawable.cachorros, "Cachorros"));
+        datos.add(new Datos(R.drawable.cerdo, "Cerdo"));
+        datos.add(new Datos(R.drawable.jirafas, "Jirafas"));
+        datos.add(new Datos(R.drawable.lobo, "Lobo"));
+        datos.add(new Datos(R.drawable.potro, "Potro"));
+        datos.add(new Datos(R.drawable.tigre, "Tigre"));
+        datos.add(new Datos(R.drawable.tortuga, "Tortuga"));
+        datos.add(new Datos(R.drawable.tucan, "Tucán"));
+
+        GridLayout contenedor = findViewById(R.id.container);
+        for(Datos dato : datos) {
+            ListView listado = new ListView(this);
+
+        }
     }
 }
