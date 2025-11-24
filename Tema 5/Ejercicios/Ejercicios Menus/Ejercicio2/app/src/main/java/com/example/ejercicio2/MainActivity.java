efb92659be84d42f1413426deb5a9ff7a41951c2
samuelvergaramartin <samuelvergaramartin@protonmail.com>
2025-11-24 18:51:13 +0100

    Updated main branch

--- Tema 5/Ejercicios/Ejercicios Menus/Ejercicio2/app/src/main/java/com/example/ejercicio2/MainActivity.java
+++ Tema 5/Ejercicios/Ejercicios Menus/Ejercicio2/app/src/main/java/com/example/ejercicio2/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.ejercicio2;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
@@ -30,6 +33,27 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Toolbar toolbar = findViewById(R.id.toolbar);
-        
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        final TextView texto = findViewById(R.id.texto);
+        int id = item.getItemId();
+        String opcion = (String) item.getTitle();
+
+        if(
+                id == R.id.lunes || id == R.id.martes || id == R.id.miercoles ||
+                id == R.id.jueves || id == R.id.viernes || id == R.id.sabado ||
+                id == R.id.domingo
+        ) texto.setText("Pulsado " + opcion);
+        else if(
+                id == R.id.enero || id == R.id.febrero || id == R.id.marzo ||
+                id == R.id.abril || id == R.id.mayo || id == R.id.junio ||
+                id == R.id.julio || id == R.id.agosto || id == R.id.septiembre ||
+                id == R.id.octubre || id == R.id.noviembre || id == R.id.diciembre
+        ) texto.setText("Pulsado el mes de " + opcion);
+
+        return super.onOptionsItemSelected(item);
     }
 }
